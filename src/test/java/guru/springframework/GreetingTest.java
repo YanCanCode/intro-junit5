416c1877373b9c0ca6d91de3f0491031d2c6e151
YanCanCode <tillett.eric@gmail.com>
2020-09-07 12:06:34 -0400

    Setup maven plugins and Junit dependencies with simple Junit5 hello world.

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    Greeting greeting;
+
+        @BeforeEach
+    void setUp() {
+        System.out.println("Clean object before each test..................");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Steve"));
+    }
+}
